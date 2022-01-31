e5cf78051d5c4cc2fe33c3f5a21623e921fbbe2a
RodrigoSester <rodrigows09@gmail.com>
2022-01-31 15:30:03 -0300

    Get statement operation test

--- src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
@@ -1,11 +1,15 @@
-import { InMemoryStatementsRepository } from "@modules/statements/repositories/in-memory/InMemoryStatementsRepository"
-import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository"
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository"
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository"
 import { GetStatementOperationUseCase } from "./GetStatementOperationUseCase"
-
 
 let usersRepositoryInMemory: InMemoryUsersRepository
 let statementRepositoryInMemory: InMemoryStatementsRepository
 let getStatementOperationUseCase: GetStatementOperationUseCase
+
+enum OperationType {
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw'
+}
 
 describe('Get statement operation', () => {
   beforeEach(() => {
@@ -14,7 +18,28 @@ describe('Get statement operation', () => {
     getStatementOperationUseCase = new GetStatementOperationUseCase(usersRepositoryInMemory, statementRepositoryInMemory)
   })
 
-  it('should be able to get statement operation', () => {
-    
+  it('should be able to get statement operation by id statement', async () => {
+    const user = {
+      name: 'test',
+      email: 'test@test.com',
+      password: 'test'
+    }
+
+    const createdUser = await usersRepositoryInMemory.create(user)
+
+    const depositStatement = {
+      user_id: createdUser.id!,
+      type: OperationType.DEPOSIT,
+      amount: 100,
+      description: 'description test'
+    }
+
+    const getStatementOperation = await statementRepositoryInMemory.create(depositStatement)
+
+    expect(getStatementOperation).toMatchObject({
+        type: 'deposit',
+        amount: 100,
+        description: 'description test'
+    })
   })
 })
