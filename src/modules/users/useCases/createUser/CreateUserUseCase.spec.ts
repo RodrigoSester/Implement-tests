d6c80a7466b19b80b8a5e9ec1b34e3e695aada8c
RodrigoSester <rodrigows09@gmail.com>
2022-01-31 11:38:46 -0300

    Email already exists test

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,11 +1,14 @@
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository"
+import { CreateUserError } from "./CreateUserError"
+import { CreateUserUseCase } from "./CreateUserUseCase"
 
-
+let createUserUseCase: CreateUserUseCase
 let usersRepositoryInMemory: InMemoryUsersRepository
 
 describe('Create user use case', () => {
   beforeEach(() => {
     usersRepositoryInMemory = new InMemoryUsersRepository()
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory)
   })
 
   it('should be able to create a new user', async () => {
@@ -15,8 +18,21 @@ describe('Create user use case', () => {
       password: 'test'
     }
 
-    const createdUser = await usersRepositoryInMemory.create(user)
+    const createdUser = await createUserUseCase.execute(user)
 
     expect(createdUser).toHaveProperty('id')
+  })
+
+  it('should not be able to create user with same email', async () => {
+    expect(async () => {
+      const user = {
+        name: 'test',
+        email: 'test@test.com',
+        password: 'test'
+      }
+
+      await createUserUseCase.execute(user)
+      await createUserUseCase.execute(user)
+    }).rejects.toBeInstanceOf(CreateUserError)
   })
 })
