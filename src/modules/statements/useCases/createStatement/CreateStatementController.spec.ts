e25b94fb0233a2999cb1d3d1f258c14d2a7c117a
RodrigoSester <rodrigows09@gmail.com>
2022-02-16 12:06:35 -0300

    Withdraw statement test

--- src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -40,8 +40,6 @@ describe('Create statement', () => {
         Authorization: `Bearer ${body.token}`
       })
 
-    console.log(response.body)
-
     expect(response.status).toBe(201)
 
     expect(response.body).toHaveProperty('user_id')
@@ -52,5 +50,33 @@ describe('Create statement', () => {
     expect(response.body.amount).toEqual(200)
     expect(response.body.type).toEqual('deposit')
     expect(response.body.description).toEqual('Churrasco')
+  })
+
+  it('should be able to make a deposit', async () => {
+    const { body } = await request(app).post('/api/v1/sessions').send({
+      email: 'test@test.com',
+      password: '61443'
+    })
+
+    const response = await request(app)
+      .post('/api/v1/statements/withdraw')
+      .send({
+        amount: 100,
+        description: 'Contas'
+      })
+      .set({
+        Authorization: `Bearer ${body.token}`
+      })
+
+    expect(response.status).toBe(201)
+
+    expect(response.body).toHaveProperty('user_id')
+    expect(response.body).toHaveProperty('id')
+    expect(response.body).toHaveProperty('updated_at')
+    expect(response.body).toHaveProperty('created_at')
+    
+    expect(response.body.amount).toEqual(100)
+    expect(response.body.type).toEqual('withdraw')
+    expect(response.body.description).toEqual('Contas')
   })
 })
