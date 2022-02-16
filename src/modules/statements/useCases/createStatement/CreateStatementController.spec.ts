daa150e82f1adf6faac24601236b5782b0e1ea3f
RodrigoSester <rodrigows09@gmail.com>
2022-02-16 12:03:01 -0300

    Create deposit statement test

---
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -0,0 +1,56 @@
+import { Connection } from 'typeorm';
+import request from 'supertest';
+import { app } from '../../../../app';
+import createConnection from '../../../../database'
+
+let connection: Connection;
+
+describe('Create statement', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    await request(app)
+      .post('/api/v1/users')
+      .send({
+        name: 'test',
+        email: 'test@test.com',
+        password: '61443'
+      })
+  })
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close()
+  })
+
+  it('should be able to make a deposit', async () => {
+    const { body } = await request(app).post('/api/v1/sessions').send({
+      email: 'test@test.com',
+      password: '61443'
+    })
+
+    const response = await request(app)
+      .post('/api/v1/statements/deposit')
+      .send({
+        amount: 200,
+        description: 'Churrasco'
+      })
+      .set({
+        Authorization: `Bearer ${body.token}`
+      })
+
+    console.log(response.body)
+
+    expect(response.status).toBe(201)
+
+    expect(response.body).toHaveProperty('user_id')
+    expect(response.body).toHaveProperty('id')
+    expect(response.body).toHaveProperty('updated_at')
+    expect(response.body).toHaveProperty('created_at')
+    
+    expect(response.body.amount).toEqual(200)
+    expect(response.body.type).toEqual('deposit')
+    expect(response.body.description).toEqual('Churrasco')
+  })
+})
