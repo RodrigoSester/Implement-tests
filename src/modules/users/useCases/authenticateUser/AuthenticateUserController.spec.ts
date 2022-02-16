c242779ef2bb1f7b443ad0a83ce3e6e2d3e5622a
RodrigoSester <rodrigows09@gmail.com>
2022-02-16 11:03:50 -0300

    Auth user tests

--- src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
@@ -1,9 +1,7 @@
 import { Connection } from 'typeorm';
 import request from 'supertest';
-
 import { app } from '../../../../app';
-import createConnection from '../../../../database';
-import { hash } from 'bcryptjs';
+import createConnection from '../../../../database'
 
 let connection: Connection;
 
@@ -32,8 +30,35 @@ describe('Authenticate User Controller', () => {
       password: '61443'
     })
 
-    console.log(response.error)
-
     expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty('token')
+  })
+
+  it('should not be able to authenticate an unexistent user', async () => {
+    const response = await request(app).post('/api/v1/sessions').send({
+      email: 'hi@latul.sd',
+      password: ''
+    })
+    
+    expect(response.status).toBe(401)
+    expect(response.body.message).toBe("Incorrect email or password")
+  })
+
+  it('should not be able to authenticate an user with incorrect password', async () => {
+    await request(app)
+      .post('/api/v1/users')
+      .send({
+        name: 'test',
+        email: 'test@test.com',
+        password: '61443'
+      })
+
+    const response = await request(app).post('/api/v1/sessions').send({
+      email: 'test@test.com',
+      password: ''
+    })
+
+    expect(response.status).toBe(401)
+    expect(response.body.message).toBe("Incorrect email or password")
   })
 })
